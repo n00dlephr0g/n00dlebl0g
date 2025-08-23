26bcfb0763dd39bfae64f174f5cfe6e596a2ae85
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-08-23 11:51:24 +1000

    Sat 23 Aug 11:51:24 AEST 2025

--- src/components/Window.js
+++ src/components/Window.js
@@ -5,22 +5,35 @@ import "../css/Window.css"
 
 export const Window = () => {
         return {
+            oninit: function(vnode) { 
+                vnode.state.view = 1;
+                vnode.state.width = 800;
+                vnode.state.height = 600;
+            },
 
             view: function(vnode) {
-                const { title, children } = vnode.attrs;
-
-
-
-                return m("div", {
-                    class: "Window"
-                }, 
-                [
-                    m("div", {class: "Titlebar"}),
-                    m("div", {
-                        class: "ContentWindow",
-                        onmousedown: vnode.state.onmousedown,
-                    }, vnode.children)
-                ]
+                return m(
+                    "div", 
+                    { 
+                        class: "Window",
+                        style: {
+                            width: `${vnode.state.width}px`,
+                            height: `${vnode.state.height}px`
+                        }
+                    }, 
+                    [
+                        // Titlebar
+                        m("div", {class: "Titlebar"}),
+                        // Content Window
+                        m(
+                            "div", 
+                            {
+                                class: "ContentWindow",
+                                onmousedown: vnode.state.onmousedown,
+                            }, 
+                            vnode.children
+                        )
+                    ]
             )
         },
     };
