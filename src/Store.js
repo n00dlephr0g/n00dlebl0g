54a86de92980103198c4c6c6b5b18e28964be51b
n00dlephr0g <monkeyjosephlee@gmail.com>
2025-08-28 00:14:39 +1000

    Thu 28 Aug 00:14:39 AEST 2025

--- src/Store.js
+++ src/Store.js
@@ -1,33 +1,44 @@
 import m from "mithril"
 
-
 let count = 0;
-export let windowStore = {
+
+export let store = {
     windows: [],
+};
 
-    defaut: {
-        windowID: 0,
-        windowState: null,
-        zindex: count,
-        windowTitle: "change me",
-        windowApp: "change me"
-    },
-
-    open_window: function (windowData)
-    {
-        windows.push(windowData);
-        count++;
-    },
-
-    close_window: function (windowID)
-    {
-        windows = windows.filter()
-    },
-
+export let windowEnum = {
     windowState: Object.freeze({
         MAXIMISED: 'max',
         WINDOWED: 'win',
         MINIMISED: 'min',
     }),
 
-}
+    windowType: Object.freeze({
+        TEST: "test"
+    })
+
+};
+
+export let windowFunctions = {
+    new: (windowTitle, windowType) => {
+        windowData = {
+            ID: 0,
+            state: windowEnum.windowState.MAXIMISED,
+            windowTitle: windowTitle, 
+            windowType: windowType,
+        };
+        store.windows.push(windowData);
+    },
+
+    close: (id) => {
+        store.windows.filter(window => window.ID == id);
+    },
+
+    to_top: (id) => {
+        target = store.windows.find(window => window.ID == id);
+        store.windows.filter(window => window.ID == window.ID);
+        store.windows.push(target);
+    },
+
+
+};
